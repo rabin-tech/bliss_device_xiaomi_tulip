13c6668df1b3653f728f38361d2ee8c81e052e13
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    tulip: Use dex2oat64 on tulip
    Enable dex2oat64 use on tulip.
    
    Bug: 153380900
    Test: boot and install an application
    (cherry picked from commit 126f03be80f57a8a0411842011152d9381589b78)
    Merged-In: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- properties.mk
+++ properties.mk
@@ -129,6 +129,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
