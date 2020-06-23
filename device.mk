a3fbf093f93852104b228a8fc7e81d74cfe88ee0
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-06-23 18:32:11 +0530

    tulip: Up-rev Health hal from 2.0 to 2.1.

--- device.mk
+++ device.mk
@@ -252,9 +252,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# Healthd
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
