b8fa324df270ef594972115c0acf0877f64531ca
Edwin Moquete <edwinmmoquete@gmail.com>
2021-03-20 21:03:54 -0400

    tulip: Move thermal configs to device tree

--- device.mk
+++ device.mk
@@ -385,6 +385,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
