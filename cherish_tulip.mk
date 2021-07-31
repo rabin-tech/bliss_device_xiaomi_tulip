eeb3c63196f8c0d8e45c82f7e2edadf942141751
rabin-tech <shrestharabin9868@gmail.com>
2021-07-31 13:25:01 +0545

    Cherish Bringup

--- cherish_tulip.mk
+++ cherish_tulip.mk
@@ -19,14 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Cherish stuff
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
 
 # Device info
-PRODUCT_NAME := aosp_tulip
+PRODUCT_NAME := cherish_tulip
 PRODUCT_DEVICE := tulip
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 6 Pro
@@ -34,6 +34,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Official
+CHERISH_BUILD_TYPE :=OFFICIAL
+
+# Maintainer
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=Rabin
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
