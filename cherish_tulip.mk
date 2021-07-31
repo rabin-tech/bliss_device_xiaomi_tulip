7c294c01b5f5ae03e446cf0e6549c54dc8556ec8
rabin-tech <60746629+rabin-tech@users.noreply.github.com>
2021-07-31 13:35:39 +0545

    Updated to July Redfin FP

--- cherish_tulip.mk
+++ cherish_tulip.mk
@@ -50,6 +50,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
