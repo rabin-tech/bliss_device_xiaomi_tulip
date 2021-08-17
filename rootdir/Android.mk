1949a7fa6032edc09391c371703b28d5c9be99bb
rabin-tech <shrestharabin9868@gmail.com>
2021-08-17 12:04:51 +0545

    Fix typo

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -93,8 +93,6 @@ LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
-LOCAL_PATH := $(call my-dir)
-
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
